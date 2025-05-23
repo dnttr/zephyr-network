1c4261017d6a8a7b335c45dfb842d05abba19c56
dnttr <147146363+dnttr@users.noreply.github.com>
2025-05-23 21:21:13 +0200

    implement PacketDecoder and PacketEncoder for Netty integration

--- src/main/java/org/dnttr/zephyr/network/codec/PacketDecoder.java
+++ src/main/java/org/dnttr/zephyr/network/codec/PacketDecoder.java
@@ -1,8 +1,57 @@
 package org.dnttr.zephyr.network.codec;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import org.dnttr.zephyr.protocol.packet.Carrier;
+import org.dnttr.zephyr.toolset.types.Type;
+
+import java.util.List;
+
 /**
  * @author dnttr
  */
 
-public class PacketDecoder {
+public class PacketDecoder extends ByteToMessageDecoder {
+
+    private final int requiredBytes = 5 * Type.INT.getBytes();
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf buffer, List<Object> objects) {
+        if (buffer.readableBytes() < requiredBytes) {
+            return;
+        }
+
+        int version = buffer.readInt();
+
+        if (version == 0x1) {
+            int identity = buffer.readInt();
+            int hash = buffer.readInt();
+            int content = buffer.readInt();
+
+            if (hash <= 0 || content <= 0) {
+                ctx.channel().disconnect();
+                return;
+            }
+
+            buffer.markReaderIndex();
+
+            if (buffer.readableBytes() < hash + content) {
+                buffer.resetReaderIndex();
+                return;
+            }
+
+            buffer.discardReadBytes(); //i think it should be discarded. TODO: check this
+
+            try {
+                objects.add(new Carrier(version, identity, hash, content, buffer.copy()));
+            } catch (Exception _) {
+                ctx.channel().disconnect();
+            } finally {
+                buffer.skipBytes(buffer.readableBytes());
+            }
+        } else {
+            ctx.channel().disconnect(); //weird things happen, don't they?
+        }
+    }
 }
