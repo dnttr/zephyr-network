15ffe9ea67d305934fdf38e92d535e93568b5ec6
dnttr <147146363+dnttr@users.noreply.github.com>
2025-06-08 16:53:27 +0200

    add Environment and Worker classes; implement session management and error handling; update pom.xml for new dependencies

--- src/main/java/org/dnttr/zephyr/network/api/Launcher.java
+++ src/main/java/org/dnttr/zephyr/network/api/Launcher.java
@@ -1,12 +1,24 @@
 package org.dnttr.zephyr.network.api;
 
+import org.dnttr.zephyr.network.bridge.ZEKit;
+
+import java.io.File;
+
 /**
  * @author dnttr
  */
 
 public class Launcher {
 
-    void launch() {
+    static int x(byte[] lol) {
+        return 0;
+    }
 
+    public static void main(String[] args) throws InterruptedException {
+        File file = new File("/Users/damian/Development/Zephyr/zephyr-network/external/libze.dylib");
+
+        System.load(file.getPath());
+        Thread.sleep(1000);
+        ZEKit.ffi_ze_close();
     }
 }
