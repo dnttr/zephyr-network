6c3ad529492fcc1dd03f462c7de4d15ac04f2347
dnttr <147146363+dnttr@users.noreply.github.com>
2025-06-16 20:20:31 +0200

    refactor ObserverManager; enhance clarity by simplifying method implementations and using var for variable declarations

--- src/main/java/org/dnttr/zephyr/network/communication/core/managers/ObserverManager.java
+++ src/main/java/org/dnttr/zephyr/network/communication/core/managers/ObserverManager.java
@@ -18,7 +18,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
  * @author dnttr
  */
 
-public class ObserverManager {
+public final class ObserverManager {
 
     private final Map<Class<? extends Packet>, List<Observer>> observers;
 
@@ -30,44 +30,37 @@ public class ObserverManager {
 
     public <T extends Packet> Observer observe(Class<T> packetClass, Direction direction, ChannelContext context)
     {
-        Observer observer = new Observer(packetClass, direction, context);
-        observers.computeIfAbsent(packetClass, k -> new CopyOnWriteArrayList<>()).add(observer);
+        var observer = new Observer(packetClass, direction, context);
+
+        this.observers.computeIfAbsent(packetClass, _
+                -> new CopyOnWriteArrayList<>()).add(observer);
 
         return observer;
     }
 
     @EventSubscriber
     public void onPacketReceived(PacketReceivedEvent ev) {
-        this.process(ev.getPacket(), ev.getChannelContext(), Direction.INBOUND);
+        this.handle(ev.getPacket(), ev.getChannelContext(), Direction.INBOUND);
     }
 
     @EventSubscriber
     public void onPacketSent(PacketSentEvent ev) {
-        this.process(ev.getPacket(), ev.getChannelContext(), Direction.OUTBOUND);
+        this.handle(ev.getPacket(), ev.getChannelContext(), Direction.OUTBOUND);
     }
 
-    private void process(Packet packet, ChannelContext context, Direction direction) {
-        Class<? extends Packet> klass = packet.getClass();
-        List<Observer> observersList = observers.get(klass);
+    private void handle(Packet packet, ChannelContext context, Direction direction) {
+        var klass = packet.getClass();
+        var observersList = observers.get(klass);
 
         if (observersList == null) {
             return;
         }
 
-        for (Observer observer : observersList) {
-            if (observer.getDirection() == direction && (observer.getContext() == null || observer.getContext().equals(context))) {
-                observer.complete(packet);
-                observersList.remove(observer);
-            }
-        }
-    }
-
-    public int getObserversCount(Class<? extends Packet> klass, Direction direction) {
-        List<Observer> observersList = observers.get(klass);
-        if (observersList == null) {
-            return 0;
-        }
-
-        return observersList.size();
+        observersList.stream()
+                .filter(observer -> observer.getDirection() == direction && (observer.getContext() == null || observer.getContext().equals(context)))
+                .forEach(observer -> {
+                    observer.complete(packet);
+                    observersList.remove(observer);
+        });
     }
 }
