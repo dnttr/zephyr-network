db770fe799b21d36078f5ed6221c4928cfb20715
dnttr <147146363+dnttr@users.noreply.github.com>
2025-06-16 20:10:34 +0200

    refactor Observer; simplify class structure by removing unused methods and enhancing clarity

--- src/main/java/org/dnttr/zephyr/network/communication/core/flow/Observer.java
+++ src/main/java/org/dnttr/zephyr/network/communication/core/flow/Observer.java
@@ -1,51 +1,26 @@
 package org.dnttr.zephyr.network.communication.core.flow;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.dnttr.zephyr.network.communication.core.channel.ChannelContext;
-import org.dnttr.zephyr.network.protocol.Packet;
 import org.dnttr.zephyr.network.communication.core.packet.processor.Direction;
-import org.jetbrains.annotations.NotNull;
+import org.dnttr.zephyr.network.protocol.Packet;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
-import java.util.function.Consumer;
-import java.util.function.Function;
 
 /**
  * @author dnttr
  */
 
+@Getter
 @RequiredArgsConstructor
 public class Observer extends CompletableFuture<Packet> {
 
+    @Getter(AccessLevel.NONE)
     private final Class<? extends Packet> klass;
 
-    @Getter
     private final Direction direction;
 
-    @Getter
     private final ChannelContext context;
-
-    @NotNull
-    public Observer accept(Consumer<? super Packet> action) {
-        return this.accept(this.klass, action);
-    }
-
-    @NotNull
-    public <U> Observer compose(Function<? super Packet, ? extends CompletionStage<U>> fn) {
-        super.thenComposeAsync(fn);
-        return this;
-    }
-
-    @NotNull
-    public Observer accept(Class<? extends Packet> klass, Consumer<? super Packet> action) {
-        super.thenAcceptAsync(msg -> {
-            if (klass.isInstance(msg)) {
-                action.accept(klass.cast(msg));
-            }
-        });
-
-        return this;
-    }
 }
